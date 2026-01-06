494ccbd2e0de366a8c56286baf2b579877d3acbe
Charlene Grangy <charlene.grangy@swissmarketplace.group>
2026-01-06 05:48:08 +0100

    Initial commit - Vite TODO app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div style={{ padding: "1rem", maxWidth: 600, margin: "0 auto" }}>
+      <h1>TODOアプリ</h1>
+      {/* <TodoList /> */}
+    </div>
+  );
+}
+
+export default App;
